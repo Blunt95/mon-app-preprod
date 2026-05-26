5728d3218ada3f4b9e5305dbfea1e7f2004041e7
Blunt95 <meressemaxence@gmail.com>
2026-05-26 13:38:30 +0200

    Update app to v2 - trigger pipeline with K6

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const http = require('http');
+﻿const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('Hello from preprod!\n');
+  res.end('Hello from preprod v2!\n');
 });
 server.listen(3000, () => console.log('Running on port 3000'));
